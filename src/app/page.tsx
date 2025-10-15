a676577a08fab961c6cdc925cd1c2deceb2e2b05
Walter Marroquin <waltermarrom777@gmail.com>
2025-10-15 12:37:45 -0600

    Add material ui library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
-import GraphStocks from './components/GraphStocks';
-import StockToWatchForm from './components/StockToWatchForm';
-import TopCards from './components/TopCards';
+import MainStocks from './components/MainStocks';
 
 export default function Home() {
 	return (
 		<div className='font-sans min-h-screen flex flex-col justify-between items-center'>
-			<main className='flex flex-col gap-4 w-full items-center md:w-10/12'>
-				<div className='w-full flex flex-col gap-4 md:flex-row'>
-					<StockToWatchForm />
-					<TopCards />
-				</div>
-				<GraphStocks />
-				<section></section>
-			</main>
+			<MainStocks />
 			<footer className='bg-red-400 flex gap-[24px] flex-wrap items-center justify-center w-full'>
 				footer
 			</footer>
