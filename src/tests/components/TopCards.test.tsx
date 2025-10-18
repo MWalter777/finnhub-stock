91575d262bb008bee65a9bb5ef889d7f1cd3a9b3
Walter Marroquin <waltermarrom777@gmail.com>
2025-10-17 21:08:51 -0600

    test Top card

---
+++ src/tests/components/TopCards.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import TopCards from '@/components/TopCards';
+
+// mock useStockContext
+jest.mock('@/Store/StockProvider', () => ({
+	useStockContext: jest.fn().mockReturnValue({
+		stockHistory: [
+			{ stock: { symbol: 'AAPL' }, history: [] },
+			{ stock: { symbol: 'GOOGL' }, history: [] },
+			{ stock: { symbol: 'MSFT' }, history: [] },
+		],
+	}),
+}));
+
+jest.mock('swiper/react', () => ({
+	Swiper: ({ children }: any) => <div>{children}</div>,
+	SwiperSlide: ({ children }: any) => <div>{children}</div>,
+}));
+
+// mock 'swiper/css'
+jest.mock('swiper/css', () => {});
+
+describe('MainStocks', () => {
+	it('renders a heading', async () => {
+		render(<TopCards />);
+
+		expect(screen.getByText('AAPL')).toBeInTheDocument();
+	});
+});
